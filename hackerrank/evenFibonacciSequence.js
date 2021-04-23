dabf4a587ba2d2cd3b380aa41359ff5f819862db
Adewale Orotayo <didymus7007@gmail.com>
2021-04-23 19:18:32 +0100

    unsolved

---
+++ hackerrank/evenFibonacciSequence.js
@@ -0,0 +1,7 @@
+const evenFibSequence = n => {
+  let a = 1, b = 2, c = sum = 0;
+  // const filt = fibSeq.filter(e => e <= n && e % 2 === 0).reduce((acc, cur) => acc + cur);
+  return c;
+}
+
+console.log(evenFibSequence(100));
